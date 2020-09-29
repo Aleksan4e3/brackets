c1260c1c0df4b33d6aa6aebc93a1c3375ea7dda4
Aleksan4e3 <aleksan4e3@gmail.com>
2020-09-29 13:48:54 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.flat();
+  let strArray = str.split('');
+  let resultArray = [];
+
+  for (const char of strArray) {
+    let index = brackets.indexOf(char);
+
+    if (brackets[index] === brackets[index + 1]) {
+      if (resultArray.includes(char)) {
+        if (resultArray.pop() !== char) return false;
+      }
+      else {
+        resultArray.push(brackets[index + 1]);
+      }
+    } else {
+      if (index % 2 == 0) resultArray.push(brackets[index + 1]);
+      else {
+        if (resultArray.pop() !== char) return false;
+      }
+    }
+
+  }
+
+  return resultArray.length === 0;
 }
